ecfac8ab377848509174360bbd5be2e40041e833
Huangney <1260371422@qq.com>
2025-11-13 20:52:31 +0800

    更新了系统与灯带
    RobotSystem更新：
    增加了自检和分区的变量
    Led更新：
    新增了static颜色，以及新的刷新模式方法

--- Sys/src/RobotSystem.cpp
+++ Sys/src/RobotSystem.cpp
@@ -15,7 +15,8 @@ void RobotSystem::Init(bool Sc)
     DWT_Init(CPU_HERT_A_BOARD_MHZ);
     
     // 初始化系统灯带
-    sys_ledband.Init(&htim5, TIM_CHANNEL_4, 15);
+    sys_ledband.Init(&htim5, TIM_CHANNEL_4, 13);
+    sys_ledband.BiasFactor = Vec3(0.843f, 1.0f, 0.843f);   // 颜色偏置因子（用于校正颜色） 
 }
 
 void RobotSystem::Run()
@@ -34,10 +35,46 @@ void RobotSystem::Update_LedBand()
 {
     static uint32_t prescaler_cnt = 0;
     prescaler_cnt++;
+
     if(prescaler_cnt >= ledband_prescaler)
     {
         prescaler_cnt = 0;
-        sys_ledband.GradientFlow(Color::Blue, Color::Red, 2.0f);
-        sys_ledband.SendData();
+        
+        // 根据不同的要求来控制系统灯
+
+        // 等待放行自检：呼吸闪烁（白灯）
+        // sys_ledband.Breath(Color::White, 1.5f);
+        sys_ledband.Expand(Color::White, 4);
+        sys_ledband.Upload();
+        
     }
+}
+
+void RobotSystem::LedBandControl()
+{
+    switch (status)
+    {
+        case PREPARE:
+        {
+            // 正常：呼吸白灯
+            if (!is_retrying)   sys_ledband.Breath(Color(255.0f, 255.0f, 255.0f), 1.0f);
+            // 重试：呼吸橙灯
+            else                sys_ledband.Breath(Color(255.0f, 127.0f, 0.0f), 1.0f);
+            break;
+        }
+        case SELF_CHECK:
+        {
+            // 自检中：白色滚动
+            sys_ledband.Running(Color(255.0f, 255.0f, 255.0f), 0.5, 1.0);
+            break;
+        }
+        case READY:
+        {
+            // 正常：固定白色常亮
+            sys_ledband.Fill(0.0f, 255.0f, 0.0f);
+            break;
+        }
+
+    }
+    
 }
